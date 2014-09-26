19f5abb7e8227f3fb4488e34fb231669572ad853
Ivan <ivan@ivan-System-Product-Name.(none)>
2014-09-26 14:26:16 +0400

    init project

---
+++ index.php
@@ -0,0 +1,29 @@
+<html lang="en" class="no-js">
+<head>
+    <meta charset="UTF-8" />
+    <title>Sticker-Note</title>
+
+    <link type="css/text" rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <div class="sticker example-1">
+        <div class="dv-text">
+            <span class="sp-text">Hello dolly! How are you? Thanks, i'm fine!</span>
+        </div>
+    </div>
+
+    <div class="sticker example-2">
+        <div class="dv-text">
+            <span class="sp-text">т.9564-46-1</span>
+        </div>
+    </div>
+
+    <script src="js/sticker.min.js"></script>
+
+    <script type="text/javascript">
+        window.onload = function() {
+            Sticker.init('.sticker');
+        }
+    </script>
+</body>
+</html>
